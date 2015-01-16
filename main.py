424b99763f3655010c704e37a346f6d0f478e23c
balamut108@gmail.com <balamut108@gmail.com>
2015-01-16 15:58:08 +0300

    Реализация турнира и статистики

--- main.py
+++ main.py
@@ -2,24 +2,21 @@
 # -*- coding: utf-8 -*-
 import service
 from random import choice, randint
-from copy import deepcopy
-import logging
+from copy import copy, deepcopy
+from logging import basicConfig, INFO, info
 
-logging.basicConfig(format=u'[%(asctime)s]  %(message)s', level=logging.INFO)
+basicConfig(format=u'[%(asctime)s]  %(message)s', level=INFO)
 
 
 class Game(object):
-	def __init__(self):
-		logging.info(u'Начало игры')
-		self.player_list = self.create_players()
+	def __init__(self, player1, player2):
+		info(u'Начало игры')
+		self.player_list = [player1, player2]
 		self.curr_player = None
+		self.player_log_list()
 
-	def create_players(self):
-		self.player_list = []
-		for u in range(2):
-			self.player_list.append(Player())
-		logging.info(u'Игроки: %s', ", ".join([x.player_name for x in self.player_list]))
-		return self.player_list
+	def player_log_list(self):
+		info(u'Игроки: %s', ", ".join([x.player_name for x in self.player_list]))
 
 	def game(self):
 		# Выбираем игрока для первого хода
@@ -32,7 +29,7 @@ class Game(object):
 		# Ходим и сохраняем результаты хода
 		shoot_res = player2.shoot(crd_for_shoot)
 		# Передаём результаты хода ходившему игроку
-		logging.info(u'Ходит: %s, координаты: %s, статус: %s', self.curr_player.player_name, crd_for_shoot, shoot_res)
+		#logging.info(u'Ходит: %s, координаты: %s, статус: %s', self.curr_player.player_name, crd_for_shoot, shoot_res)
 		self.curr_player.return_shoot_state(shoot_res, crd_for_shoot)
 		# Меняем счётчик текущего пользователя, если ходивший промазал
 		if shoot_res == u'Мимо!':
@@ -40,9 +37,13 @@ class Game(object):
 		# Конец игры и вывод статистики
 		elif shoot_res == u'Убил последний корабль!':
 			self.curr_player.scores += 1
-			logging.info(u'Выйграл: %s', self.curr_player.player_name)
-			logging.info(u'%s', ", ".join([str(x.player_name) + u" набрал очков:  " + str(x.scores) + u", ходов: " + str(x.steps) for x in self.player_list]))
-			return
+			info(u'Выйграл: %s', self.curr_player.player_name)
+			info(u'%s', ", ".join([str(x.player_name) + u" набрал очков:  " + str(x.scores) + u", ходов: " + str(x.steps) for x in self.player_list]))
+			# Подготавливаем к следующей игре
+			stats.get_stats(self.player_list)
+			self.curr_player.reset_values()
+			info(u'------------------')
+			return self.curr_player
 		# Если игра продолжается, то перезапускаем функцию game()
 		return self.game()
 
@@ -58,6 +59,7 @@ class Player(object):
 		self.succ_shoots = []
 		self.scores = 0
 		self.steps = 0
+		self.tur_scores = 0
 
 	def create_ships(self):
 		self.ships = []
@@ -149,6 +151,17 @@ class Player(object):
 			for ind_for_del in reversed(del_index[ship]):
 				del self.combinations[ship][ind_for_del]
 
+	def reset_values(self):
+		self.combinations = deepcopy(service.GLOBAL_DATA)
+		self.create_ships()
+		self.alien = []
+		self.recomendation_pool = []
+		self.ships_defeat = []
+		self.succ_shoots = []
+		self.tur_scores += self.scores
+		self.scores = 0
+		self.steps = 0
+
 
 class Ship(object):
 	def __init__(self, ship_type, cord, halo):
@@ -165,5 +178,38 @@ class Ship(object):
 			self.state = u'Попал!'
 		return self.state
 
+class Statistic(object):
+	def __init__(self):
+		self.step_all = []
+		self.step_winners = []
+		self.scores_loosers = []
+
+	def get_stats(self, player_list):
+		for pl in player_list:
+			self.step_all.append(pl.steps)
+			if len(pl.ships_defeat) == 10:
+				self.scores_loosers.append(pl.scores)
+			else:
+				self.step_winners.append(pl.steps)
+
+	def count_middles(self):
+		return sum(self.step_all)/float(len(self.step_all)), sum(self.step_winners)/float(len(self.step_winners)), sum(self.scores_loosers)/float(len(self.scores_loosers))
+
+
 if __name__ == '__main__':
-	Game().game()
+	stats = Statistic()
+	turnaiment_player_counter = 1024
+	info(u'Начало турнира')
+	tur_player_list = [Player() for player in range(turnaiment_player_counter)]
+	info(u'Список игроков: %s', ", ".join([x.player_name for x in tur_player_list]))
+	tur_player_list_next_iter = []
+	while len(tur_player_list) != 1:
+		for player_ind in range(1, len(tur_player_list), 2):
+			winner = Game(tur_player_list[player_ind-1], tur_player_list[player_ind]).game()
+			tur_player_list_next_iter.append(winner)
+		tur_player_list = copy(tur_player_list_next_iter)
+		tur_player_list_next_iter = []
+	else:
+		info(u'Турнир выйграл: %s, набрал очков: %s', tur_player_list[0].player_name, tur_player_list[0].tur_scores)
+	med_step_all, med_step_win, med_score_looser = stats.count_middles()
+	info(u'Статистика: \n\t1. Среднее количесво ходов: %.2f,\n\t2. Среднее количество ходов выйгравших игроков: %.2f,\n\t3. Среднее количество очков, которое набрали проигравшие: %.2f', med_step_all, med_step_win, med_score_looser)
