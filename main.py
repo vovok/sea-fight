acb087036fd6b5e17783b15731168b7f710775c9
balamut108@gmail.com <balamut108@gmail.com>
2015-01-20 18:24:06 +0300

    Реализация статистики и доп. статегий ходов

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import service
-from random import choice, randint
+from random import choice, randint, shuffle
 from copy import copy, deepcopy
 from logging import basicConfig, INFO, info
 
@@ -10,40 +10,47 @@ basicConfig(format=u'[%(asctime)s]  %(message)s', level=INFO)
 
 class Game(object):
 	def __init__(self, player1, player2):
-		info(u'Начало игры')
+		#info(u'Начало игры')
 		self.player_list = [player1, player2]
 		self.curr_player = None
 		self.player_log_list()
 
 	def player_log_list(self):
-		info(u'Игроки: %s', ", ".join([x.player_name for x in self.player_list]))
+		#info(u'Игроки: %s', ", ".join([x.player_name for x in self.player_list]))
+		pass
 
 	def game(self):
+		tour_stats.game_id += 1
 		# Выбираем игрока для первого хода
 		if self.curr_player is None:
 			self.curr_player = choice(self.player_list)
 		# Получаем координаты для хода
-		crd_for_shoot = self.curr_player.step()
+		crd_for_shoot = self.curr_player.strategy.get_crd_for_step()
 		# Выделяем второго игрока из списка
 		player2 = filter(lambda x: x != self.curr_player, self.player_list)[0]
 		# Ходим и сохраняем результаты хода
 		shoot_res = player2.shoot(crd_for_shoot)
 		# Передаём результаты хода ходившему игроку
 		#logging.info(u'Ходит: %s, координаты: %s, статус: %s', self.curr_player.player_name, crd_for_shoot, shoot_res)
-		self.curr_player.return_shoot_state(shoot_res, crd_for_shoot, player2)
+		self.curr_player.strategy.return_shoot_state(shoot_res, crd_for_shoot, player2)
+		self.curr_player.stat.step += 1
+		if shoot_res in [u'Убил!', u'Попал!']:
+			self.curr_player.stat.score += 1
 		# Меняем счётчик текущего пользователя, если ходивший промазал
 		if shoot_res == u'Мимо!':
 			self.curr_player = player2
 		# Конец игры и вывод статистики
-		elif shoot_res == u'Убил последний корабль!':
-			self.curr_player.scores += 1
-			info(u'Выйграл: %s', self.curr_player.player_name)
-			info(u'%s', ", ".join([str(x.player_name) + u" набрал очков:  " + str(x.scores) + u", ходов: " + str(x.steps) for x in self.player_list]))
-			# Подготавливаем к следующей игре
-			stats.get_stats(self.player_list)
-			self.curr_player.reset_values()
-			info(u'------------------')
-			return self.curr_player
+		if shoot_res == u'Убил!':
+			self.curr_player.stat.ships_defeat.append(1)
+			if len(self.curr_player.stat.ships_defeat) == 10:
+				#info(u'Выйграл: %s', self.curr_player.player_name)
+				#info(u'%s', ", ".join([str(x.player_name) + u" набрал очков:  " + str(x.scores) + u", ходов: " + str(x.steps) for x in self.player_list]))
+				# Сбрасываем счётчики
+				self.curr_player.stat.tur_scores += self.curr_player.stat.score
+				tour_stats.get_stats(self.player_list)
+				self.curr_player.reset_values()
+				#info(u'------------------')
+				return self.curr_player
 		# Если игра продолжается, то перезапускаем функцию game()
 		return self.game()
 
@@ -51,28 +58,22 @@ class Game(object):
 class Player(object):
 	def __init__(self):
 		self.player_name = service.rdn_usr_name()
-		self.combinations = deepcopy(service.GLOBAL_DATA)
+		self.strategy = PlayerStrategy()
+		self.stat = PlayerStatistic()
 		self.ships = self.create_ships()
-		self.alien = []
-		self.recomendation_pool = []
-		self.ships_defeat = []
-		self.succ_shoots = []
-		self.scores = 0
-		self.steps = 0
-		self.tur_scores = 0
 
 	def create_ships(self):
 		self.ships = []
 		buff_cord = []
 		ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
 		for ship in ships:
-			if self.combinations[ship]:
-				cords = choice(self.combinations[ship])
+			if self.strategy.combinations[ship]:
+				cords = choice(self.strategy.combinations[ship])
 				overlay = service.set_halo(cords)
-				self.data_cleaner(cords, overlay)
+				self.strategy.data_cleaner(cords, overlay)
 				buff_cord.append([ship, cords, overlay])
 			else:
-				self.combinations = deepcopy(service.GLOBAL_DATA)
+				self.strategy.reload()
 				return self.create_ships()
 		for cords_for_unpack in buff_cord:
 			ship, cords, overlay = cords_for_unpack
@@ -85,40 +86,111 @@ class Player(object):
 			if cords in ship.cord:
 				ship.shoots.append(cords)
 				shoot_res = ship.get_state()
-				if shoot_res == u'Убил!':
-					self.ships_defeat.append(1)
-					if len(self.ships_defeat) == 10:
-						return u'Убил последний корабль!'
 				return shoot_res
 		else:
 			return u'Мимо!'
 
-	def step(self):
-		"""Выбор координат для хода"""
-		if not self.recomendation_pool:
-			x, y = randint(0, 9), randint(0, 9)
-			if [x, y] not in self.alien:
-				self.alien.append([x, y])
-				self.steps += 1
-				return [x, y]
-			else:
-				return self.step()
+	def reset_values(self):
+		self.strategy.reset()
+		self.stat.reset()
+		self.ships = self.create_ships()
+
+class Ship(object):
+	def __init__(self, ship_type, cord, halo):
+		self.ship_type = ship_type
+		self.cord = cord
+		self.halo = halo
+		self.shoots = []
+		self.state = u'Цел'
+
+	def get_state(self):
+		if len(self.shoots) == self.ship_type:
+			self.state = u'Убил!'
 		else:
+			self.state = u'Попал!'
+		return self.state
+
+class PlayerStatistic(object):
+	def __init__(self):
+		self.score = 0
+		self.step = 0
+		self.ships_defeat = []
+		self.tur_scores = 0
+
+	def reset(self):
+		self.score = 0
+		self.step = 0
+		self.ships_defeat = []
+
+
+
+class TournaimentStatistic(object):
+	def __init__(self):
+		self.game_id = 0
+		self.step_all = []
+		self.step_winners = []
+		self.scores_loosers = []
+		self.step_loosers = []
+		self.players_copy_list = []
+
+	def get_stats(self, player_list):
+		self.players_copy_list.extend([deepcopy(player) for player in player_list])
+
+
+	def count_middles(self):
+		self.step_all = [player.stat.step for player in self.players_copy_list]
+		self.step_winners = [player.stat.step for player in self.players_copy_list if len(player.stat.ships_defeat) == 10]
+		self.step_loosers = [player.stat.step for player in self.players_copy_list if len(player.stat.ships_defeat) != 10]
+		self.scores_loosers = [player.stat.score for player in self.players_copy_list if len(player.stat.ships_defeat) != 10]
+		return sum(self.step_all)/float(len(self.step_all)), sum(self.step_winners)/float(len(self.step_winners)), sum(self.step_loosers)/float(len(self.step_loosers)), sum(self.scores_loosers)/float(len(self.scores_loosers))
+
+	def startegy_effect(self):
+		report_strategy = {u"Победители":[], u"Проигравшие":[]}
+		for player in self.players_copy_list:
+			pl_stat = ""
+			if len(player.stat.ships_defeat) == 10:
+				pl_stat = u"Победители"
+			else:
+				pl_stat = u"Проигравшие"
+			report_strategy[pl_stat].append([player.strategy.ships_strategy_collocation, player.strategy.steps_strategy])
+		return report_strategy
+
+
+
+class PlayerStrategy(object):
+	def __init__(self):
+		self.alien_cords = []
+		self.recomendation_pool = []
+		self.succ_shoots = []
+		self.ships_strategy_collocation = STRATEGY_QUOTA.pop()
+		self.combinations = deepcopy(service.gen_cord(self.ships_strategy_collocation))
+		self.steps_strategy = choice(service.STEPS_STRATEGY.keys())
+		self.steps_cords = deepcopy(service.STEPS_STRATEGY[self.steps_strategy])
+
+	def get_crd_for_step(self):
+		"""Выбор координат для хода"""
+		if self.recomendation_pool:
 			crd = self.recomendation_pool.pop(0)
-			self.alien.append(crd)
-			self.steps += 1
-			return crd
+		elif self.steps_cords:
+			shuffle(self.steps_cords)
+			crd = self.steps_cords.pop(0)
+		else:
+			crd = choice(filter(lambda x: x not in self.alien_cords, service.CORD_10_10))
+		if crd in self.recomendation_pool:
+			self.recomendation_pool.remove(crd)
+		elif crd in self.recomendation_pool:
+			self.recomendation_pool.remove(crd)
+		self.alien_cords.append(crd)
+		return crd
 
 	def return_shoot_state(self, state, crd, player2):
 		"""Стратегия дальнейщих ходов в зависимости от результата текущего хода"""
 		if state == u'Попал!':
-			self.scores += 1
 			if not self.recomendation_pool:
 				crd_rec = [[crd[0] - 1, crd[1]], [crd[0] + 1, crd[1]], [crd[0], crd[1] - 1], [crd[0], crd[1] + 1]]
 				crd_rec = filter(lambda x: 0 <= x[0] <= 9 and 0 <= x[1] <= 9, crd_rec)
-				crd_rec = filter(lambda x: x not in self.alien, crd_rec)
 				self.succ_shoots.append(crd)
-				self.recomendation_pool.extend(crd_rec)
+				self.recomendation_pool.extend([crd for crd in crd_rec if crd not in self.alien_cords])
 			else:
 				crd_s1 = self.recomendation_pool[0]
 				crd_s2 = self.succ_shoots[0]
@@ -129,16 +201,14 @@ class Player(object):
 						else:
 							crd_rec = [[crd_s1[ind]-1, crd_s1[ind]-2], [crd_s2[ind]+1, crd_s2[ind]+2]]
 						crd_rec = filter(lambda x: 0 <= x[0] <= 9 and 0 <= x[1] <= 9, crd_rec)
-						crd_rec = filter(lambda x: x not in self.alien, crd_rec)
-						self.recomendation_pool.extend(crd_rec)
+						self.recomendation_pool.extend([crd for crd in crd_rec if crd not in self.alien_cords])
 		elif state == u'Убил!':
 			for ship in player2.ships:
 				if crd in ship.cord:
-					self.alien.extend(ship.halo)
-			self.scores += 1
+					self.alien_cords.extend([crd for crd in ship.halo if crd not in self.alien_cords])
+					self.steps_cords = filter(lambda x: x not in ship.halo and x not in self.alien_cords, self.steps_cords)
 			self.recomendation_pool = []
 			self.succ_shoots = []
-
 
 	def data_cleaner(self, cords, overlay):
 		"""Удаляет использованные комбинации из словаря комбинаций пользователя
@@ -153,60 +223,32 @@ class Player(object):
 		for ship in del_index.keys():
 			for ind_for_del in reversed(del_index[ship]):
 				del self.combinations[ship][ind_for_del]
+	def reload(self):
+		self.combinations = deepcopy(service.gen_cord(self.ships_strategy_collocation))
 
-	def reset_values(self):
-		self.combinations = deepcopy(service.GLOBAL_DATA)
-		self.create_ships()
-		self.alien = []
+	def reset(self):
+		self.alien_cords = []
 		self.recomendation_pool = []
-		self.ships_defeat = []
 		self.succ_shoots = []
-		self.tur_scores += self.scores
-		self.scores = 0
-		self.steps = 0
-
-
-class Ship(object):
-	def __init__(self, ship_type, cord, halo):
-		self.ship_type = ship_type
-		self.cord = cord
-		self.halo = halo
-		self.shoots = []
-		self.state = u''
-
-	def get_state(self):
-		if len(self.shoots) == self.ship_type:
-			self.state = u'Убил!'
-		else:
-			self.state = u'Попал!'
-		return self.state
-
-class Statistic(object):
-	def __init__(self):
-		self.step_all = []
-		self.step_winners = []
-		self.scores_loosers = []
-		self.step_loosers = []
-
-	def get_stats(self, player_list):
-		for pl in player_list:
-			self.step_all.append(pl.steps)
-			if len(pl.ships_defeat) == 10:
-				self.scores_loosers.append(pl.scores)
-				self.step_loosers.append(pl.steps)
-			else:
-				self.step_winners.append(pl.steps)
-
-	def count_middles(self):
-		return sum(self.step_all)/float(len(self.step_all)), sum(self.step_winners)/float(len(self.step_winners)), sum(self.step_loosers)/float(len(self.step_loosers)), sum(self.scores_loosers)/float(len(self.scores_loosers))
-
+		self.combinations = deepcopy(service.gen_cord(self.ships_strategy_collocation))
+		self.steps_strategy = choice(service.STEPS_STRATEGY.keys())
+		self.steps_cords = deepcopy(service.STEPS_STRATEGY[self.steps_strategy])
 
 if __name__ == '__main__':
-	stats = Statistic()
 	turnaiment_player_counter = 1024
+	tour_stats = TournaimentStatistic()
 	info(u'Начало турнира')
+	STRATEGY_QUOTA = [y for y in ["for_1_ship_left",
+	                             "for_1_ship_right",
+	                             "for_1_ship_top",
+	                             "for_1_ship_bottom",
+	                             "for_1_ship_center_horisontal",
+	                             "for_1_ship_center_vertical",
+	                             "for_1_ship_36",
+	                             "random_12"] for x in range(turnaiment_player_counter/8)]
+	shuffle(STRATEGY_QUOTA)
 	tur_player_list = [Player() for player in range(turnaiment_player_counter)]
-	info(u'Список игроков: %s', ", ".join([x.player_name for x in tur_player_list]))
+	#info(u'Список игроков: %s', ", ".join([x.player_name for x in tur_player_list]))
 	tur_player_list_next_iter = []
 	while len(tur_player_list) != 1:
 		for player_ind in range(1, len(tur_player_list), 2):
@@ -215,6 +257,14 @@ if __name__ == '__main__':
 		tur_player_list = copy(tur_player_list_next_iter)
 		tur_player_list_next_iter = []
 	else:
-		info(u'Турнир выйграл: %s, набрал очков: %s', tur_player_list[0].player_name, tur_player_list[0].tur_scores)
-	med_step_all, med_step_win, med_step_looser, med_score_looser = stats.count_middles()
+		info(u'Турнир выйграл: %s, набрал очков: %s', tur_player_list[0].player_name, tur_player_list[0].stat.tur_scores)
+	med_step_all, med_step_win, med_step_looser, med_score_looser = tour_stats.count_middles()
 	info(u'Статистика: \n\t1. Среднее количесво ходов (всех игроков): %.2f,\n\t2. Среднее количество ходов выйгравших игроков: %.2f,\n\t3. Среднее количество ходов проигравших игроков: %.2f,\n\t4. Среднее количество очков, которое набрали проигравшие: %.2f', med_step_all, med_step_win, med_step_looser,med_score_looser)
+	res_strat = tour_stats.startegy_effect()
+	for pl_stat in res_strat.keys():
+		info(u'%s:', pl_stat)
+		bf = []
+		for strategy_com in res_strat[pl_stat]:
+			if strategy_com not in bf:
+				bf.append(strategy_com)
+				info(u'%s: %s', ", ".join(strategy_com), res_strat[pl_stat].count(strategy_com))
--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 #!python
 # -*- coding: utf-8 -*-
-from random import randint
+from random import randint, choice
 from itertools import permutations as comb
 
 
@@ -31,10 +31,16 @@ def set_halo(cords):
 				halo.append(ads)
 	return filter(lambda x: 0 <= x[0] <= 9 and 0 <= x[1] <= 9, halo)
 
-def gen_cord():
-	"""Генератор всех возможных комбинаций координат"""
-	all_comb = [[x/10, x % 10] for x in range(100)]
-	for_1_ship = filter(lambda x: x[0] in range(2, 8) and x[1] in range(2, 8), all_comb)
+def gen_cord(strategy):
+	"""Генератор всех возможных комбинаций координат + генерирует случайную стратегию из 12 координат"""
+	all_comb, cords_for_1_ship = GLOBAL_DATA
+	GLOBAL_DATA[1]["random_12"] = []
+	while len(GLOBAL_DATA[1]["random_12"]) != 12:
+		x_cord = randint(0, 9)
+		y_cord = randint(0, 9)
+		if [x_cord, y_cord] not in GLOBAL_DATA[1]["random_12"]:
+			GLOBAL_DATA[1]["random_12"].append([x_cord, y_cord])
+	for_1_ship = cords_for_1_ship[strategy]
 	for_other_ship = filter(lambda x: x not in for_1_ship, all_comb)
 	cord_comb = {1: [[x] for x in for_1_ship], 2: [], 3: [], 4: []}
 	for ship in filter(lambda x: x != 1, cord_comb.keys()):
@@ -49,4 +55,44 @@ def gen_cord():
 					cord_comb[ship].append(dir_d)
 	return cord_comb
 
-GLOBAL_DATA = gen_cord()
+def get__cord_for_1_ship():
+	"""Генерирует 7 кобинайций расстановки однопалубных и возвращает случайную комбинацию"""
+	cord_for_1_ship = {
+	"for_1_ship_left": filter(lambda x: x[0] in range(0, 6) and x[1] in range(0, 10), CORD_10_10),
+	"for_1_ship_right": filter(lambda x: x[0] in range(4, 10) and x[1] in range(0, 10), CORD_10_10),
+	"for_1_ship_top": filter(lambda x: x[0] in range(0, 10) and x[1] in range(0, 6), CORD_10_10),
+	"for_1_ship_bottom": filter(lambda x: x[0] in range(0, 10) and x[1] in range(4, 10), CORD_10_10),
+	"for_1_ship_center_horisontal": filter(lambda x: x[0] in range(2, 10) and x[1] in range(2, 8), CORD_10_10),
+	"for_1_ship_center_vertical": filter(lambda x: x[0] in range(2, 8) and x[1] in range(0, 10), CORD_10_10),
+	"for_1_ship_36": filter(lambda x: x[0] in range(2, 8) and x[1] in range(2, 8), CORD_10_10)}
+	return CORD_10_10, cord_for_1_ship
+
+def gen_cross_cord():
+	cross_cord = []
+	cross_cord.extend([[x, x] for x in range(10)])
+	cross_cord.extend([[9-x, x] for x in range(10)])
+	cross_cord.extend([[x, y] for x in range(5, 6) for y in range(1, 10, 2)])
+	cross_cord.extend([[x, y] for x in range(4, 5) for y in range(0, 10, 2)])
+	cross_cord.extend([[x, y] for y in range(5, 6) for x in range(1, 10, 2)])
+	cross_cord.extend([[x, y] for y in range(4, 5) for x in range(0, 10, 2)])
+	del_cords = [[x, y] for x in range(4, 6) for y in range(4, 6)]
+	for del_crd in del_cords:
+		while 1:
+			if del_crd in cross_cord:
+				cross_cord.remove(del_crd)
+			else:
+				break
+	return cross_cord
+
+def gen_linear_cord_var_2():
+	"""http://habrahabr.ru/post/180995/ вариант 2"""
+	linear_cord = []
+	linear_cord.extend([[3-x, 0+x] for x in range(4)])
+	linear_cord.extend([[7-x, 0+x] for x in range(8)])
+	linear_cord.extend([[2+x, 9-x] for x in range(8)])
+	linear_cord.extend([[6+x, 9-x] for x in range(4)])
+	return linear_cord
+
+CORD_10_10 = [[x/10, x % 10] for x in range(100)]
+GLOBAL_DATA = get__cord_for_1_ship()
+STEPS_STRATEGY = {"cross": gen_cross_cord(), "linear": gen_linear_cord_var_2(), "random": []}
